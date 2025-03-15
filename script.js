b512c107f0af976a48006f27e33b09ac93554702
Yashwanth S <yashwanths814@gmail.com>
2025-03-15 22:20:12 +0530

    Add initial HTML, CSS, and JavaScript files for Namma Yatri Mobility

---
+++ script.js
@@ -0,0 +1,5 @@
+function login(userType) {
+    alert(`Redirecting to ${userType} login page...`);
+    // Here, you can add redirection logic, e.g.,
+    // window.location.href = `${userType}.html`;
+}
